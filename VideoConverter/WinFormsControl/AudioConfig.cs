fcbdbe70077f9cba2a974ec7207ba708b0a70da7
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2024-12-25 21:44:12 +0900

    ユーザーコントロールを追加

---
+++ VideoConverter/WinFormsControl/AudioConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsControl
+{
+    public partial class AudioConfig : UserControl
+    {
+        public AudioConfig()
+        {
+            InitializeComponent();
+        }
+    }
+}
